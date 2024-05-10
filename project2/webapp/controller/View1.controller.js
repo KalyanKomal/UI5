d020810f28dcefa0925cdb494c9c9b6bfe5647b1
Pentapalli Kalyan Komal <pentapalli.kalyan@incture.com>
2024-05-10 06:55:01 +0000

    Added the global busy indicator

--- project2/webapp/controller/View1.controller.js
+++ project2/webapp/controller/View1.controller.js
@@ -1,16 +1,42 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller","sap/ui/model/json/JSONModel"
+    "sap/ui/core/mvc/Controller","sap/ui/model/json/JSONModel","sap/ui/core/BusyIndicator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,JSONModel) {
+    function (Controller,JSONModel,BusyIndicator) {
         "use strict";
 
         return Controller.extend("com.incture.project2.controller.View1", {
             onInit: function () {
-                var oModel=new JSONModel("model/data.json");
-                this.getView().setModel(oModel);
-            }
+              /*  var oModel=new JSONModel("model/data.json");
+                this.getView().setModel(oModel);*/
+            },show4000 : function() {
+                this.showBusyIndicator(4000);
+            },
+    
+            show4000_0 : function() {
+                this.showBusyIndicator(4000, 0);
+            },
+    
+            show1000_2000 : function() {
+                this.showBusyIndicator(8000, 2000);
+            },showBusyIndicator:function(iDuration,iDelay){
+                //IT WILL SHOW THE DELAY AFTER iDelay
+                BusyIndicator.show(iDelay);
+                if(iDuration>0){
+                    //REMOVING ANY PREVIOUS INSTANCE OF TIMEOUT
+                    if(this._sTimeoutId){
+                        clearTimeout(this._sTimeoutId);
+                        this._sTimeoutId=null;
+                    }
+                //ADDING THE TIMEOUT
+				this._sTimeoutId = setTimeout(function() {
+					this.hideBusyIndicator();
+				}.bind(this), iDuration);
+                }
+            },	hideBusyIndicator : function() {
+                BusyIndicator.hide();
+            },
         });
     });
