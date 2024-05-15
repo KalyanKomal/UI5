7b86f99608b539c6033c4f1c76564455454e2772
Pentapalli Kalyan Komal <pentapalli.kalyan@incture.com>
2024-05-15 09:11:27 +0000

    Added NotificationsGroup

--- project2/webapp/controller/View1.controller.js
+++ project2/webapp/controller/View1.controller.js
@@ -1,11 +1,13 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller","sap/ui/model/json/JSONModel","sap/ui/core/BusyIndicator"
+    "sap/ui/core/mvc/Controller","sap/ui/model/json/JSONModel","sap/ui/core/BusyIndicator","sap/m/MessageToast","sap/ui/core/library",	"sap/m/NotificationListItem"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,JSONModel,BusyIndicator) {
+    function (Controller,JSONModel,BusyIndicator,MessageToast,coreLibrary,NotificationListItem) {
         "use strict";
+        var Priority=coreLibrary.Priority;
+        console.log(Priority);
 
         return Controller.extend("com.incture.project2.controller.View1", {
             onInit: function () {
@@ -82,6 +84,54 @@ sap.ui.define([
                 var sSplitAppMode = oEvent.getSource().getSelectedButton().getCustomData()[0].getValue();
     console.log(sSplitAppMode);
                 this.getSplitAppObj().setMode(sSplitAppMode);
+            },	onListItemPress: function (oEvent) {
+                MessageToast.show("Item Pressed: " + oEvent.getSource().getTitle());
+            },onItemClose: function (oEvent) {
+                var oItem = oEvent.getSource(),
+                    oList = oItem.getParent();
+    
+                oList.removeItem(oItem);
+                MessageToast.show("Item Closed: " + oItem.getTitle());
+            },onRejectPress: function () {
+                MessageToast.show("Reject Button Pressed");
+            },
+    
+            onAcceptPress: function () {
+                MessageToast.show("Accept Button Pressed");
+            },onLoadNotificationsPress: function (oEvent) {
+                var oNotificationListGroup = oEvent.getSource().getParent().getParent();
+                var iMaxNotifications = 400;
+    
+                if (!oNotificationListGroup.getItems().length) {
+                    for (var i = 1; i <= iMaxNotifications; i++) {
+                        oNotificationListGroup.addItem(this._createNotification(i));
+                    }
+                }
+            },
+    
+            _createNotification: function (iIndex) {
+                function randomIndex(iMax) {
+                    return Math.floor(Math.random() * iMax);
+                }
+    
+                var aPriorities = Object.keys(Priority);//Priority Object9High,Medium,Low,None
+                var aTimes = ["3 days", "5 minutes", "1 hour"];
+                var aTitles = ["New order request", "Your vacation has been approved", "New transaction in queue", "An new request await your action"];
+                var sPriority = aPriorities[randomIndex(aPriorities.length)];
+    
+                return new NotificationListItem({
+                    title: aTitles[randomIndex(aTitles.length)] + " " + iIndex,
+                    showCloseButton: true,
+                    datetime: aTimes[randomIndex(aTimes.length)],
+                    unread: true,
+                    priority: Priority[sPriority],
+                    //CloseButton
+                    close: function() {
+                        this.destroy();
+                    }
+                });
             }
+    
+    
         });
     });
